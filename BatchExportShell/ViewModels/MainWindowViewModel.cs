f66258b54088ad9b02ba96ddcd1ce9f4ecc6aecc
lcmoore <lcmoore@protonmail.com>
2023-08-15 18:28:19 -0700

    Add project files.

---
+++ BatchExportShell/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace BatchExportShell.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+
+        public DelegateCommand<string> NavigateCommand { get; set; }
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+          
+            
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+            _regionManager = regionManager;
+
+        }
+
+        private void Navigate(string uri)
+        {
+            _regionManager.RequestNavigate("ContentRegion", uri);
+        }
+
+    }
+}
---
+++ BatchExportShell/Views/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace BatchExportShell.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // Enables the titlebar to be drag-moved on mousedown
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        // makes the minimize button work
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+
+        }
+
+        // makes the maximize button work
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+    }
+
+}
