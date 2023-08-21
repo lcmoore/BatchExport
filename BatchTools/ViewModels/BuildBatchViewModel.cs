92f0a73f4afee2f2959659b1633b0c2e77e05516
lcmoore <lcmoore@protonmail.com>
2023-08-20 23:11:05 -0700

    Import Async

--- BatchTools/ViewModels/BuildBatchViewModel.cs
+++ BatchTools/ViewModels/BuildBatchViewModel.cs
@@ -24,6 +24,12 @@ namespace BatchTools.ViewModels
     public class BuildBatchViewModel : BindableBase
     {
         #region Declarations
+        private bool _backendReady = true;
+        public bool BackendReady
+        {
+            get { return _backendReady; }
+            set { SetProperty(ref _backendReady, value); }
+        }
         private ObservableCollection<object> _selectedItem = new ObservableCollection<object>();
 
         public ObservableCollection<object> SelectedItem
@@ -33,16 +39,8 @@ namespace BatchTools.ViewModels
         }
 
         public object _treeViewSelectionChanged { get; set; }
-        private string _addItemText = "Loading Summaries...";
+       
 
-        public string AddItemText
-        {
-            get { return _addItemText; }
-            set { SetProperty(ref _addItemText, value); }
-        }
-
-        private ObservableCollection<UISummary> _summaries;
-        public ObservableCollection<UISummary> Summaries { get { return _summaries; } set { SetProperty(ref _summaries, value); } }
 
 
         // collections declaration
@@ -61,12 +59,6 @@ namespace BatchTools.ViewModels
             set { SetProperty(ref _tmpCurrentResults, value); }
         }
 
-
-        private readonly IDialogService _dialogService;
-
-
-
-
         private IEventAggregator _eventAggregator;
 
 
@@ -74,7 +66,7 @@ namespace BatchTools.ViewModels
         #endregion
         #region Command Declarations
 
-        public DelegateCommand ImportQueryCommand { get; private set; }
+        public DelegateCommand<object> ImportQueryCommand { get;  set; }
         public DelegateCommand SendQueryCommand { get; private set; }
         public DelegateCommand EclipseQueryCommand { get; private set; }
         public DelegateCommand AddQueryItemCommand { get; private set; }
@@ -90,8 +82,8 @@ namespace BatchTools.ViewModels
 
         public BuildBatchViewModel(IDialogService dialogService, IEventAggregator eventAggregator)
         {
-            ImportQueryCommand = new DelegateCommand(ImportQuery);
-            AddQueryItemCommand = new DelegateCommand(AddQueryItem);
+            ImportQueryCommand = new DelegateCommand<object>(ImportQueryAsync).ObservesCanExecute(()=>BackendReady);
+
 
             StackPanelClickCommand = new DelegateCommand<object>(StackPanelClick);
 
@@ -100,7 +92,7 @@ namespace BatchTools.ViewModels
 
             _eventAggregator = eventAggregator;
 
-            _dialogService = dialogService;
+        
 
 
  
@@ -142,24 +134,6 @@ namespace BatchTools.ViewModels
 
 
 
-        private void AddQueryItem()
-        {
-            var p = new DialogParameters();
-            p.Add("Summaries", Summaries.ToList());
-            // _dialogService.Show(~) would allow users to continue to interact with the base view 
-            // while dialog is open
-            _dialogService.Show("AddQueryItemDialog", p, result =>
-            {//callback
-                if (result.Result == ButtonResult.OK)
-                {
-
-                }
-
-
-
-            });
-
-        }
 
 
 
@@ -289,6 +263,136 @@ namespace BatchTools.ViewModels
 
             
 
+        }
+        private async void ImportQueryAsync(Object obj)
+        {
+
+
+            // if the output file already exists, delete it
+            if (File.Exists(@"PythonScripts/tmp/output.json"))
+            {
+                File.Delete(@"PythonScripts/tmp/output.json");
+            }
+            Dictionary<string, Dictionary<string, List<string>>> request = new Dictionary<string, Dictionary<string, List<string>>>();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    BackendReady = false;
+
+                    string[] lines = File.ReadAllLines(openFileDialog.FileName);
+                    foreach (string line in lines)
+                    {
+                        string course = null;
+                        string plan = null;
+
+
+                        string[] columns = line.Split(',');
+
+
+                        string id = columns.ElementAt(0);
+                        // add leading 0 if necessary, since csv removes leading 0s
+                        if (columns.ElementAt(0).Length == 7)
+                        {
+                            id = "0" + columns.ElementAt(0);
+                        }
+                        // check if columns length is > 1
+                        if (columns.Length > 1)
+                        {
+                            course = columns.ElementAt(1);
+                        }
+
+                        // check if columns length is > 2
+                        if (columns.Length > 2)
+                        {
+                            plan = columns.ElementAt(2);
+                        }
+                        // if id not in requests keys, create a new object
+                        if (!request.ContainsKey(id))
+                        {
+                            request.Add(id, new Dictionary<string, List<string>>());
+                        }
+                        // if course not in requests[id] keys, create a new object
+                        if (course != null && !request[id].ContainsKey(course))
+                        {
+                            request[id].Add(course, new List<string>());
+                        }
+                        // if plan not in requests[id][course] keys, add it
+                        if (course != null && plan != null && !request[id][course].Contains(plan))
+                        {
+                            request[id][course].Add(plan);
+                        }
+
+
+                    }
+                    List<Patient> patient_requests = new List<Patient>();
+                    foreach (KeyValuePair<string, Dictionary<string, List<string>>> entry in request)
+                    {
+                        Patient thisPatient = new Patient(entry.Key, entry.Value);
+                        patient_requests.Add(thisPatient);
+
+
+
+                    }
+                    // convert request dictionary to json
+                    string json = JsonSerializer.Serialize(patient_requests);
+                    // write json to file
+                    File.WriteAllText(@"PythonScripts/tmp/request.json", json);
+                    // run python script
+
+                    string result = await Task.Run(()=>callPythonHelper());
+                    // read in the json file and create a list of patients
+                    string json_input = File.ReadAllText(@"PythonScripts/tmp/output.json");
+                    List<Patient> patients = JsonSerializer.Deserialize<List<Patient>>(json_input)!;
+                    // replace the CurrentResults with the new results
+                    CurrentResults = new ObservableCollection<Patient>(patients);
+                    BackendReady = true;
+
+
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                    BackendReady = true;
+                }
+
+
+
+            }
+
+
+
+
+
+
+
+        }
+        private string callPythonHelper()
+        {
+            string result = "";
+            System.Diagnostics.ProcessStartInfo start = new System.Diagnostics.ProcessStartInfo();
+            //python interprater location
+            start.FileName = "PythonScripts/venv/Scripts/python.exe";
+            //argument with file name and input parameters
+            start.Arguments = string.Format("{0}", "PythonScripts/GetPatients.py");
+            start.UseShellExecute = false;// Do not use OS shell
+            start.CreateNoWindow = true; // We don't need new window
+            start.RedirectStandardOutput = true;// Any output, generated by application will be redirected back
+            start.RedirectStandardError = true; // Any error in standard output will be redirected back (for example exceptions)
+            start.LoadUserProfile = true;
+            using (System.Diagnostics.Process process = System.Diagnostics.Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string stderr = process.StandardError.ReadToEnd(); // Here are the exceptions from our Python script
+                    result =  reader.ReadToEnd(); // Here is the result of StdOut(for example: print "test")
+
+
+                }
+            }
+            return result;
         }
         #endregion
 
